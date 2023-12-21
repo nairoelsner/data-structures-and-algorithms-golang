6285efcb4c70daeb173a3128b852ee5625fa685b
Nairo Elsner <nairo.elsner@hotmail.com>
2023-12-21 11:29:51 -0300

    add insertion sort

--- src/algorithms/sort_algorithms/sort_algorithms.go
+++ src/algorithms/sort_algorithms/sort_algorithms.go
@@ -15,3 +15,15 @@ func SelectionSort(list *[]int) {
 
 	}
 }
+
+func InsertionSort(list *[]int) {
+	for i := 1; i < len(*list); i++ {
+		key := (*list)[i]
+		j := i - 1
+		for j >= 0 && (*list)[j] > key {
+			(*list)[j+1] = (*list)[j]
+			j--
+		}
+		(*list)[j+1] = key
+	}
+}
